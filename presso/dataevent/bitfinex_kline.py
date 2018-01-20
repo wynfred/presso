bfdf155a6e96a22a3675c2a97afc0abff45c883b
Fred <wynfred@126.com>
2018-01-19 20:56:09 -0500

    bitfinex realtime kline data

--- presso/dataevent/bitfinex_kline.py
+++ presso/dataevent/bitfinex_kline.py
@@ -4,7 +4,7 @@ import json
 import numpy
 
 from presso.core.abstract.dataevent import AbstractDataEvent
-from presso.core.util import HttpSession, timeframeToSeconds
+from presso.core.util import isRealtime, HttpSession, timeframeToSeconds
 
 
 class BitfinexKlineDataEvent(AbstractDataEvent):
@@ -14,10 +14,14 @@ class BitfinexKlineDataEvent(AbstractDataEvent):
                                                self._config['market'])
         # Bitfinex uses microseconds instead of seconds
         self.__timeframe = timeframeToSeconds(self._config['time_frame']) * 1000
-        self.__now = self._config['start_time'] * 1000 // self.__timeframe * self.__timeframe
-        self.__end_time = self._config['end_time'] * 1000 // self.__timeframe * self.__timeframe
+        if isRealtime():
+            self._iter = self.__realtime
+        else:
+            self.__now = self._config['start_time'] * 1000 // self.__timeframe * self.__timeframe
+            self.__end_time = self._config['end_time'] * 1000 // self.__timeframe * self.__timeframe
+            self._iter = self.__history
 
-    async def _iter(self):
+    async def __history(self):
         while self.__end_time > self.__now:
             # Batch size of 100
             params = {'start': self.__now,
@@ -34,6 +38,18 @@ class BitfinexKlineDataEvent(AbstractDataEvent):
                     self.__now += self.__timeframe
             # Server limit
             await asyncio.sleep(6)
+
+    async def __realtime(self):
+        last_tstamp = 0
+        while True:
+            async with HttpSession.get().get(self.__url,
+                                             params={'limit': 2},
+                                             verify_ssl=False) as resp:
+                tick = json.loads(await resp.text())[1]
+                if last_tstamp != tick[0]:
+                    last_tstamp = tick[0]
+                    yield self.__parse(tick)
+            await asyncio.sleep(self.__timeframe / 2000)
 
     def __parse(self, resp):
         # Use history data if candle is missing from server
