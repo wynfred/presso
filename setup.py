875c1f6a3cbdfad91a14261bd341f20708a59872
Yongnan Wu <ywu@atlmbp266.ad.pdrop.net>
2018-06-29 17:14:53 -0400

    add setuptool and reconstruct project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='presso',
+    version='1.0.0',
+    description='Event-driven backtest/realtime quantitative trading system.',
+    packages=find_packages(exclude=('*.test',
+                                    '*.test.*',
+                                    'test.*',
+                                    'test',
+                                    'docs')),
+    entry_points={
+        'console_scripts': [
+            'presso = presso.cli.presso:run',
+        ]
+    },
+)
